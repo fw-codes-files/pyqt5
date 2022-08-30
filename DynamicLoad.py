4795359b502bbfd904a4dfd7c54ab5260b113e0d
fw <615552878@qq.com>
2022-08-30 21:45:58 +0800

    make code more flexible

--- DynamicLoad.py
+++ DynamicLoad.py
@@ -40,10 +40,11 @@ class MainWindow(QDialog):
 
     def show_camera(self):
         flag, self.image = self.cap.read()  # 从视频流中读取
+        print(self.image.shape)
         # todo 根据ak的代码改变一下showImage部分ok了
-        show = cv2.resize(self.image, (640, 480))  # 把读到的帧的大小重新设置为 640x480
+        show = cv2.resize(self.image, (450, 256))  # 把读到的帧的大小重新设置
         show = cv2.cvtColor(show, cv2.COLOR_BGR2RGB)  # 视频色彩转换回RGB，这样才是现实的颜色
-        showImage = QtGui.QImage(show.data, show.shape[1], show.shape[0],
+        showImage = QtGui.QImage(show.data, show.shape[1], show.shape[0],show.shape[1]*3,
                                  QtGui.QImage.Format_RGB888)  # 把读取到的视频数据变成QImage形式
         self.ui.master.setPixmap(QtGui.QPixmap.fromImage(showImage))  # 往显示视频的Label里 显示QImage
 
